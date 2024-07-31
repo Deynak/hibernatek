96c37c0d52d28a937a9d68dd2ea5d69ce190f96d
Deynak <miron-ox@mail.ru>
2024-08-01 01:40:52 +0300

    finall commit

--- src/main/java/com/deynak/util/Util.java
+++ src/main/java/com/deynak/util/Util.java
@@ -7,44 +7,13 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
 
-    static String url = "jdbc:mysql://localhost:3306/mydbtest";
-    static String user = "bestuser";
-    static String password = "bestuser";
-    static Connection connection = null;
-    private static SessionFactory sessionFactory;
+    private static SessionFactory sessionFactory = null;
 
-    public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(url, user, password);
-            // Your database interaction logic here
-            if (connection != null) {
-                System.out.println("Connection successful");
-            }
-            connection.setAutoCommit(false);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            connection.close();
-        }
-        return connection;
-    }
 
-    public static void closeConnection() throws SQLException {
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static SessionFactory getConnectionHibernate(){
         if (sessionFactory == null) {
@@ -52,6 +21,7 @@ public class Util {
                 Configuration configuration = new Configuration();
 
                 Properties settings = new Properties();
+
                 settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
                 settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mydbtest");
                 settings.put(Environment.USER, "bestuser");
@@ -72,16 +42,5 @@ public class Util {
             }
         }
         return sessionFactory;
-    }
-
-    public static void closeSessionFactoryHibernate() throws SQLException {
-        try {
-            if (sessionFactory != null) {
-                sessionFactory.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
     }
 }
